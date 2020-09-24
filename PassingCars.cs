e9976dba9511fff89108da356b9afb3315239a07
HunKimADev <hun.kim@outlook.com>
2020-09-24 15:10:56 +1200

    Solution for the PassingCars Quiz

---
+++ PassingCars.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+
+/*
+A non-empty array A consisting of N integers is given. The consecutive elements of array A represent consecutive cars on a road.
+
+Array A contains only 0s and/or 1s:
+
+0 represents a car traveling east,
+1 represents a car traveling west.
+The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
+
+For example, consider array A such that:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given a non-empty array A of N integers, returns the number of pairs of passing cars.
+
+The function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000.
+
+For example, given:
+
+  A[0] = 0
+  A[1] = 1
+  A[2] = 0
+  A[3] = 1
+  A[4] = 1
+the function should return 5, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer that can have one of the following values: 0, 1.
+*/
+
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        var counter = new int[A.Length];
+        var eastIndexes = new List<int>();
+        
+        for(int i = 0; i < A.Length; i ++){
+            
+            if(i > 0){
+                counter[i] += counter[i-1];
+            }
+            
+            if(A[i] == 0)
+                eastIndexes.Add(i);
+            else
+                counter[i]++;
+        }
+        
+        int result = 0;
+        
+        for(int i = 0; i < eastIndexes.Count; i++){
+            int currIndex = eastIndexes[i];
+            result += counter[A.Length-1] - counter[currIndex];
+            if(result > 1000000000){
+                return -1;
+            }
+        }
+        
+        return result;
+    }
+}
